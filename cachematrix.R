f8b4a0792bcbc55ebe1d40d5f512582fdae3079f
matt-aya <majd.atyah@gmail.com>
2015-07-24 12:39:15 +0800

    fixing typos and clean my comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,47 +1,32 @@
 
-
 ## The following are two functions:
-
 ## They cache and compute the inverse of a matrix.
 
-
                 ## FIRST : Function one:
-
 
 ## 1. Creates a special "matrix" object
 ## 2. compute it's inverse with --- solve() ---
 ## 3. cache the result into getinv
 
-
 makeCacheMatrix <- function(mat = matrix()) {
   inverse <- NULL
   set <- function(x) {
-    
-    # assign variable mat to the matrix
-    # assign Empty variable to inverse
+        # assign variable mat to the matrix
+        # assign Empty variable to inverse
     mat <<- x;
     inverse <<- NULL;
   }
-  
   get <- function() mat;
-  
-  # Very Important :
-  # use --- solve() --- to calulate the inverse
-  
+      # Very Important :
+      # use --- solve() --- to calulate the inverse
   setinv <- function(inv) inverse <<- solve(mat);
-  
-  # now cache the inverse into getinv
-  
+      # now cache the inverse into getinv
   getinv <- function() inverse;
-  
-  # Assignthe variables
-  
+      # Assignthe variables
   list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
-
                 ## SECOND : Function two:
-
 
 ## computes the inverse of the special "matrix" returned by
 ## the previous function makeCacheMatrix .
@@ -50,48 +35,32 @@ makeCacheMatrix <- function(mat = matrix()) {
 ## then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(mat, ...) {
-  
-  # 1. check if the inverse has been calculated
-  
+      # 1. check if the inverse has been calculated
   inverse <- mat$getinv()
   if(!is.null(inverse)) {
     message("Getting cached data")
     return(inverse)
   }
-  
-  # 2. else retrieve the inverse from the cache use ---solve()---
-  
+      # 2. else retrieve the inverse from the cache use ---solve()---
   data <- mat$get()
   invserse <- solve(data)
   mat$setinv(inverse)
   return(inverse)
 }
 
-
-
                 ## Now time to try it all out
 
-
-## 1. create a Square Invertible matrix
-
+    ## 1. create a Square Invertible matrix
 c <- rbind(c(3,5),c(-7,2))
-
-## 2. assign the first function
-
+    ## 2. assign the first function
 a <- makeCacheMatrix()
-
-## 3. call set and get and check the variable assignment
-
+    ## 3. call set and get and check the variable assignment
 a$set(c)
 a$get()
-
-## 4. Now the FUN part: inverse and check the result
-
+    ## 4. Now the FUN part: inverse and check the result
 a$setinv(c)
 a$getinv()
-
-## 5. Second function, assign and check the cached result.
-
+    ## 5. Second function, assign and check the cached result.
 e <- cacheSolve(a)
 e
 
